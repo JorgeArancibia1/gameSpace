495859c4d0149875138ec0dae54f71b0a1bd2383
koke <jorge.arancibia.jeal@gmail.com>
2019-11-28 11:07:23 -0300

    Tercera parte, Eventos agregados

--- main.js
+++ main.js
@@ -13,6 +13,10 @@ var fondo;
    width: 50,
    height: 50
  }
+ //CREAMOS OBJETO TECLADO VACÍO
+var teclado = {
+
+}
 
 
 // Trae la imagen y llama al frameloop que a su vez llama la función para dibujar el fondo.
@@ -36,6 +40,28 @@ function dibujarNave() {
   ctx.fillRect(nave.x, nave.y, nave.width, nave.height );//Dibujamos un rectangulo(x,y,ancho,alto)
 }
 
+function agregarEventosTeclado() {
+  //Ejecutamos la función agregarEventos
+  //Lo que hace esta en particular es agregar el numero de codigo de tecla al objeto "teclado" y lo va a colocar en "true" (teclado[32] = true;)
+  agregarEvento(document, "keydown", function(e){ //Agregamos al documento el evento keydown, que cuando se presione una tecla se ejecuta la función.
+    teclado[e.keyCode] = true; //Va a tomar el objeto del teclado y va a agregar como clave del evento el keyCode("Codigo que identifica la tecla presionada").
+  });//Es como representar "teclado.n = true"(tecla n° x encendida)
+
+  //PARA DEJAR EN FALSO LA TECLA QUE DEJÓ DE SER PRESIONADA.
+  agregarEvento(document,"keyup", function(e) {
+    teclado[e.keyCode] = false;
+  });
+
+  //Programamos la función agregarEventos
+  function agregarEvento(elemento, nombreEvento, funcion) {
+    if (element.addEventListener) { //Si existe addEventListener utilizalo.
+      elemento.addEventListener(nombreEvento,funcion,false); //Aquí se asigna el elemento al cual se va a escuchar, que evento se ejecutara, que es lo que hará cuando se accione el evento y que inicie el escucha en falso.
+    } else if (elemento.attachEvent) { //Para que funcione en internet explorer.
+      elemento.attachEvent(nombreEvento, funcion);
+    }
+  }
+}
+
 //Se encarga de actualizar todas las posiciones de los jugadores y va a redibujar cada
 // uno de los elementos del juego para el movimiento, además de dibujar el background.
 function frameLoop() {
@@ -43,4 +69,7 @@ function frameLoop() {
  dibujarNave();
 }
 
+
+//EJECUCIÓN DE FUNCIONES
 loadMedia();
+agregarEventosTeclado();
