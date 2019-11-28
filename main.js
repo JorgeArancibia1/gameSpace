d1cf47fdca714cc97e1f611f87b38086add2ac80
koke <jorge.arancibia.jeal@gmail.com>
2019-11-28 10:40:20 -0300

    Segunda parte, Creacion de la nave

--- main.js
+++ main.js
@@ -6,8 +6,16 @@ var fondo;
 
 
 
+// CREAR EL OBJETO DE LA NAVE
+ var nave = {
+   x: 100,
+   y: canvas.height-100,//El alto del canvas - 100px
+   width: 50,
+   height: 50
+ }
 
 
+// Trae la imagen y llama al frameloop que a su vez llama la función para dibujar el fondo.
 function loadMedia() {
   fondo = new Image();
   fondo.src = 'space.jpg';
@@ -16,15 +24,23 @@ function loadMedia() {
   }
 }
 
-
-function drawBackground() {
+//DIBUJA EL FONDO
+function dibujarFondo() {
   ctx.drawImage(fondo, 0, 0);
+}
+
+function dibujarNave() {
+  //Guarda un punto en la pila del contexto(checkpoint, guarda la información actual que posee).
+  ctx.save();
+  ctx.fillStyle= 'white'; //Pinta de blanco
+  ctx.fillRect(nave.x, nave.y, nave.width, nave.height );//Dibujamos un rectangulo(x,y,ancho,alto)
 }
 
 //Se encarga de actualizar todas las posiciones de los jugadores y va a redibujar cada
 // uno de los elementos del juego para el movimiento, además de dibujar el background.
 function frameLoop() {
- drawBackground();
+ dibujarFondo();
+ dibujarNave();
 }
 
 loadMedia();
